7742c2789db19af8c5fbb04114b15e228af5a59a
David Shepherd <davidshepherd7@gmail.com>
2014-11-21 12:31:03 +0000

    Refactor FCFSPower main function

--- examples/org/workflowsim/examples/scheduling/FCFSPower.java
+++ examples/org/workflowsim/examples/scheduling/FCFSPower.java
@@ -2,31 +2,11 @@
 package org.workflowsim.examples.scheduling;
 
 import java.io.File;
-import java.util.Calendar;
-import java.util.List;
-
-import org.cloudbus.cloudsim.Log;
-import org.cloudbus.cloudsim.core.CloudSim;
-import org.cloudbus.cloudsim.Host;
-import org.workflowsim.WorkflowDatacenter;
-import org.workflowsim.Job;
-import org.workflowsim.WorkflowEngine;
-import org.workflowsim.WorkflowPlanner;
-import org.workflowsim.utils.ClusteringParameters;
-import org.workflowsim.utils.OverheadParameters;
-import org.workflowsim.utils.Parameters;
-import org.workflowsim.utils.ReplicaCatalog;
-
-
-
-// Probably redundant, fix?
-import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
-
 import org.cloudbus.cloudsim.Cloudlet;
 import org.cloudbus.cloudsim.CloudletSchedulerSpaceShared;
 import org.cloudbus.cloudsim.DatacenterCharacteristics;
@@ -39,20 +19,6 @@ import org.cloudbus.cloudsim.Vm;
 import org.cloudbus.cloudsim.VmAllocationPolicySimple;
 import org.cloudbus.cloudsim.VmSchedulerTimeShared;
 import org.cloudbus.cloudsim.core.CloudSim;
-import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
-import org.workflowsim.ClusterStorage;
-import org.workflowsim.WorkflowDatacenter;
-import org.workflowsim.Job;
-import org.workflowsim.WorkflowEngine;
-import org.workflowsim.WorkflowPlanner;
-import org.workflowsim.utils.ClusteringParameters;
-import org.workflowsim.utils.OverheadParameters;
-import org.workflowsim.utils.Parameters;
-import org.workflowsim.utils.ReplicaCatalog;
-
-
 import org.cloudbus.cloudsim.power.PowerDatacenter;
 import org.cloudbus.cloudsim.power.PowerDatacenterBroker;
 import org.cloudbus.cloudsim.power.PowerHost;
@@ -61,105 +27,121 @@ import org.cloudbus.cloudsim.power.PowerVm;
 import org.cloudbus.cloudsim.power.PowerVmAllocationPolicyMigrationAbstract;
 import org.cloudbus.cloudsim.power.models.PowerModel;
 import org.cloudbus.cloudsim.power.models.PowerModelCubic;
+import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
+import org.workflowsim.ClusterStorage;
+import org.workflowsim.Job;
+import org.workflowsim.WorkflowDatacenter;
+import org.workflowsim.WorkflowEngine;
+import org.workflowsim.WorkflowPlanner;
+import org.workflowsim.utils.ClusteringParameters;
+import org.workflowsim.utils.OverheadParameters;
+import org.workflowsim.utils.Parameters;
+import org.workflowsim.utils.ReplicaCatalog;
 
 /**
- * This FCFS Scheduling Algorithm with power aware stuff
+ * The FCFS Scheduling Algorithm with power aware stuff
  */
 public class FCFSPower {
-    // extends HEFTPlanningAlgorithmExample1 {
 
-    ////////////////////////// STATIC METHODS ///////////////////////
     /**
-     * Creates main() to run this example This example has only one datacenter
-     * and one storage
+     * Creates main() to run this example This example has only one
+     * datacenter and one storage.
      */
     public static void main(String[] args) {
 
 
-        try {
-            // First step: Initialize the WorkflowSim package. 
-
-            /**
-             * However, the exact number of vms may not necessarily be vmNum If
-             * the data center or the host doesn't have sufficient resources the
-             * exact vmNum would be smaller than that. Take care.
-             */
-            int vmNum = 5; //number of vms;
-            String daxPath = "config/dax/Montage_100.xml";
-            
-            
+	// First step: Initialize the WorkflowSim package.
+	// ============================================================
+	{
+            // Use FCFS
             Parameters.SchedulingAlgorithm sch_method = Parameters.SchedulingAlgorithm.FCFS;
+
+            // No planning because FCFS is a dynamic scheduling algorithm
             Parameters.PlanningAlgorithm pln_method = Parameters.PlanningAlgorithm.INVALID;
+
+            // ??ds
             ReplicaCatalog.FileSystem file_system = ReplicaCatalog.FileSystem.LOCAL;
 
-            /**
-             * No overheads 
-             */
+            // No overheads
             OverheadParameters op = new OverheadParameters(0, null, null, null, null, 0);;
-            
-            /**
-             * No Clustering
-             */
-            ClusteringParameters.ClusteringMethod method = ClusteringParameters.ClusteringMethod.NONE;
-            ClusteringParameters cp = new ClusteringParameters(0, 0, method, null);
 
-            /**
-             * Initialize static parameters
-             */
-            Parameters.init(vmNum, daxPath, null,
+            // No Clustering
+            ClusteringParameters cp = new ClusteringParameters
+                (0, 0, ClusteringParameters.ClusteringMethod.NONE, null);
+
+            // The exact number of vms may not necessarily be vmNum. If
+            // the data center or the host doesn't have sufficient
+            // resources the exact vmNum would be smaller than that.
+            // Take care. ??ds there's no need for this to be in a
+            // static object, only used here!
+            int vmNum = 5;
+
+            // Initialize static Parameters object
+            Parameters.init(vmNum, "config/dax/Montage_100.xml", null,
                             null, op, cp, sch_method, pln_method,
                             null, 0);
             ReplicaCatalog.init(file_system);
+        }
 
-            // before creating any entities.
-            int num_user = 1;   // number of grid users
-            Calendar calendar = Calendar.getInstance();
-            boolean trace_flag = false;  // mean trace events
 
-            // Initialize the CloudSim library
-            CloudSim.init(num_user, calendar, trace_flag);
+        // Initialize the CloudSim library
+        // ============================================================
+        {
+            // number of grid users
+            int num_user = 1;
 
+            // should we write a CloudSim trace file?
+            boolean trace_flag = false;
+
+            CloudSim.init(num_user, Calendar.getInstance(), trace_flag);
+        }
+
+
+        // Initialise the rest
+        // ============================================================
+        WorkflowEngine wfEngine = null;
+        {
+            // Create a datacenter that can handle workflows
             WorkflowDatacenter datacenter0 = createDatacenter("Datacenter_0");
 
-            /**
-             * Create a WorkflowPlanner with one schedulers.
-             */
-            WorkflowPlanner wfPlanner = new WorkflowPlanner("planner_0", 1);
-            /**
-             * Create a WorkflowEngine.
-             */
-            WorkflowEngine wfEngine = wfPlanner.getWorkflowEngine();
-            /**
-             * Create a list of VMs.The userId of a vm is basically the id of
-             * the scheduler that controls this vm.
-             */
-            List<Vm> vmlist0 = 
+            // Create a WorkflowPlanner with one scheduler
+            WorkflowPlanner wfPlanner = null;
+            try {
+                wfPlanner = new WorkflowPlanner("planner_0", 1);
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            // Create a WorkflowEngine.
+            wfEngine = wfPlanner.getWorkflowEngine();
+
+            // Create a list of VMs.The userId of a vm iss basically the
+            // id of the scheduler that controls this vm.
+            List<Vm> vmlist0 =
                 createVM(wfEngine.getSchedulerId(0), Parameters.getVmNum());
 
-            /**
-             * Submits this list of vms to this WorkflowEngine.
-             */
+            // Submits this list of vms to this WorkflowEngine.
             wfEngine.submitVmList(vmlist0, 0);
 
-            /**
-             * Binds the data centers with the scheduler.
-             */
+            // Binds the data centers with the scheduler.
             wfEngine.bindSchedulerDatacenter(datacenter0.getId(), 0);
+        }
 
+
+        // Simulate!
+        // ============================================================
+        {
             CloudSim.startSimulation();
-
 
             List<Job> outputList0 = wfEngine.getJobsReceivedList();
 
             CloudSim.stopSimulation();
 
             printJobList(outputList0);
-
-
-        } catch (Exception e) {
-            Log.printLine("The simulation has been terminated due to an unexpected error");
-            throw new RuntimeException(e);
         }
+
     }
 
 
@@ -186,7 +168,7 @@ public class FCFSPower {
             long storage = 1000000; //host storage
             int bw = 10000;
             PowerModel powerModel = new PowerModelCubic(1.0, 0.3);
-            
+
             hostList.add
                 (new PowerHost(hostId++,
                                new RamProvisionerSimple(ram),
@@ -218,11 +200,10 @@ public class FCFSPower {
 
 
         // 6. Finally, we need to create a storage object.
-        /**
-         * The bandwidth within a data center in MB/s.
-         */
+
+	// The bandwidth within a data center in MB/s.
         int maxTransferRate = 15;// the number comes from the futuregrid site, you can specify your bw
-        
+
         try {
             HarddriveStorage s1 = new HarddriveStorage(name, 1e12);
             s1.setMaxTransferRate(maxTransferRate);
@@ -251,8 +232,8 @@ public class FCFSPower {
         for(int i = 0; i < vms; i++) {
             list.add
                 (new PowerVm
-                 (i, 
-                  userId, 
+                 (i,
+                  userId,
                   mips,
                   pesNumber,
                   ram,
